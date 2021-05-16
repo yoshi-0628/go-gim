a86059bb3d38299e49e1578bc35a7977d23acba8
yoshi-0628 <59409129+yoshi-0628@users.noreply.github.com>
2021-05-16 09:31:09 +0900

    screen追加

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -1,23 +1,10 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import WeightScreen from '../screens/WeightScreen';
+import HomeScreen from '../screens/HomeScreen';
+import SettingScreen from '../screens/SettingScreen';
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
-
-function SettingsScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Settings Screen</Text>
-    </View>
-  );
-}
 const Tab = createMaterialBottomTabNavigator();
 
 function TabNavigator() {
@@ -27,11 +14,22 @@ function TabNavigator() {
       shifting
     >
       <Tab.Screen
+        name="Weight"
+        component={WeightScreen}
+        options={{
+          tabBarLabel: 'Weight',
+          tabBarColor: 'blue',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="scale" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
         name="Home"
         component={HomeScreen}
         options={{
           tabBarLabel: 'Home',
-          tabBarColor: 'black',
+          tabBarColor: '#a83275',
           tabBarIcon: ({ color }) => (
             <MaterialCommunityIcons name="home" color={color} size={26} />
           ),
@@ -39,12 +37,12 @@ function TabNavigator() {
       />
       <Tab.Screen
         name="Setting"
-        component={SettingsScreen}
+        component={SettingScreen}
         options={{
-          tabBarColor: '#a83275',
+          tabBarColor: 'black',
           tabBarLabel: 'Setting',
           tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="bell" color={color} size={26} />
+            <MaterialCommunityIcons name="cog" color={color} size={26} />
           ),
         }}
       />
